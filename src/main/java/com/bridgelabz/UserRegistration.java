e0dc5ce6d109e75deaf1d37c128a6b3176023f83
Surabhi951 <surabhibhagat95@gmail.com>
2022-12-08 20:00:29 +0530

    Refactor: code to use lambda function to validate user entry

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,6 +5,10 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
+    /*
+    valid first name
+    first name should start with a Cap and should have minimum 3 characters
+     */
     Validator firstName = (firstName) -> {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -19,6 +23,10 @@ public class UserRegistration {
         }
     };
 
+    /*
+    valid last name
+    last name should start with a Cap and should have minimum 3 characters
+     */
     Validator lastName = (lastName) -> {
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -34,6 +42,10 @@ public class UserRegistration {
         }
     };
 
+    /*
+    valid email
+    check email address:- abc.xyz@bl.co.in
+     */
     Validator email = (email) -> {
 
         String regex = "^[a-z]*.[a-z]+@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$";
@@ -50,6 +62,10 @@ public class UserRegistration {
         }
     };
 
+    /*
+    pre-defined mobile format
+    mobile number should be use country code follow with space and 10 digits number
+     */
     Validator mobileNumber = (mobileNumber) -> {
         String regex = "^[\\d]{2}\\s[\\d]{10}$";
         Pattern pattern = Pattern.compile(regex);
@@ -65,6 +81,10 @@ public class UserRegistration {
         }
     };
 
+    /*
+    pre-defined password rule1
+    Password must have 8 character
+     */
     Validator passwordRule1 = (passwordRule1) -> {
         String regex = "^[A-Za-z0-9@._-]{8,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -80,6 +100,7 @@ public class UserRegistration {
         }
     };
 
+    //Password must have atleast one uppercase letter
     Validator passwordRule2 = (passwordRule2) -> {
         String regex = "^[A-Z]{1}[A-Za-z0-9@._-]{7,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -95,6 +116,7 @@ public class UserRegistration {
         }
     };
 
+    //Password must have atleast one numeric number
     Validator passwordRule3 = (passwordRule3) -> {
         String regex = "^(?=.*[A-z])(?=.*[0-9])([a-zA-Z0-9@._-]).{8,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -110,6 +132,7 @@ public class UserRegistration {
         }
     };
 
+    //Password must has exactly one special character
     Validator passwordRule4 = (passwordRule4) -> {
         String regex = "^(?=.*[A-z])(?=.*[0-9])(?=.*[@#$%^&*()-+=])([a-zA-Z0-9@._-]).{8,}$";
         Pattern pattern = Pattern.compile(regex);
--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class UserRegistrationMain {
 
     public static void main(String[] args) throws InvalidUserDetailsException {
-        UserRegistration userRegistration = new UserRegistration();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the options:\n1. Validate First Name\n2. Validate Last Name\n3. Validate Email ID\n4. Validate Mobile Number\n5. Validate Password Rule1\n6. Validate Password Rule2\n7. Validate Password Rule3\n8. Validate Password Rule4");
-        switch (scanner.nextInt()){
+        UserRegistration userRegistration = new UserRegistration();//create an object of user registration class
+        Scanner scanner = new Scanner(System.in);//create an object of scanner class
+        System.out.println("Enter the options:\n1. Validate First Name\n2. Validate Last Name\n3. Validate Email ID\n4. Validate Mobile Number\n5. Validate Password Rule1\n6. Validate Password Rule2\n7. Validate Password Rule3\n8. Validate Password Rule4");//select option
+        switch (scanner.nextInt()) {
             case 1:
                 userRegistration.firstName.validate("Surabhi");
                 break;
